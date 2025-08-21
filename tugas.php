d615af0081e96fab7d256812a10f899f80924883
Agizz-svg <Argies2007>
2025-08-21 15:13:17 +0700

    CRUD commit

--- tugas.php
+++ tugas.php
@@ -1,5 +1,64 @@
 <?php
     include "jaringan.php";
+    
+    
+    $action = $_POST['action'] ?? '';
+    
+if($action === 'tambah'){
+    header('Content-Type: application/json');
+    $nama = $koneksi->real_escape_string($_POST['nama'] ?? '');
+    $alamat = $koneksi->real_escape_string($_POST['alamat'] ?? '');
+
+if ($nama && $alamat) {
+    $insert = $koneksi->query("INSERT INTO warga (nama, alamat) VALUES ('$nama', '$alamat')");
+    if ($insert) {
+        $id = $koneksi->insert_id;
+        echo json_encode([
+            'status' => 'success',
+            'id' => $id,
+            'nama' => $nama,
+            'alamat' => $alamat,
+        ]);
+    } else {
+        echo json_encode(['status' => 'error', 'pesan' => $koneksi->error]);
+    }
+} else {
+    echo json_encode(['status' =>'error', 'pesan' => 'Nama/alamat kosong']);
+}
+exit;
+}
+
+
+    $action = $_POST['action'] ?? '';
+    $keyword = $_POST['keyword'] ?? '';
+
+    // Hapus Data
+    if ($action == 'hapus') {
+        $id = intval($_POST['id']);
+        $hapus = $koneksi->query("DELETE FROM warga WHERE id = $id");
+            echo "success";
+        exit;
+    }
+    
+    // Ambil data untuk edit
+    if ($action == "getData"){
+        $id = intval($_POST['id']);
+        $result = $koneksi->query("SELECT * FROM warga WHERE id=$id");
+        echo json_encode($result->fetch_assoc());
+        exit;
+    }
+
+    // Update Data
+    if ($action == "update"){
+        $id     = intval($_POST['id']);
+        $nama   = $koneksi->real_escape_string($_POST['nama']);
+        $alamat = $koneksi->real_escape_string($_POST['alamat']);
+        $update = $koneksi->query("UPDATE warga SET nama='$nama', alamat='$alamat' WHERE id=$id");
+        echo "success";
+        exit;
+    }
+
+    // Pencarian Data
 
     $keyword = $_POST['keyword'] ?? '';
 
@@ -15,12 +74,14 @@
         $no = 1;
         while ($row = $result->fetch_assoc()) {
             echo "
-                <tr>
+                <tr id='row-$row[id]'>
                         <td>$no</td> 
-                        <td>{$row['nama']}</td>
-                        <td>{$row['alamat']}<td>
-                        <button onclick=\"return confirmHapus({$row['id']})\">Hapus</button>
-                        <button onclick=\"alert('Apakah anda ingin megeditnya?')\">Edit</button>
+                        <td class='nama_$row[id]'>$row[nama]</td>
+                        <td class='alamat_$row[id]'>$row[alamat]</td>
+                        <td>
+                        <button class='hapusBtn' data-id='$row[id]'>Hapus</button>
+                        <button class='editBtn' data-id='$row[id]'>Edit</button>
+                        </td>
                 </tr>
                 ";
                 $no++;
